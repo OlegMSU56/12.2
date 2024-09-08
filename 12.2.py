135f471bdd6ab2142f0c346adc5ce4ee6cd1f37f
Oleg <sagdeev.ol@yandex.ru>
2024-09-08 15:41:32 +0300

    Add files via upload

---
+++ 12.2.py
@@ -0,0 +1,32 @@
+import unittest
+import run_2
+
+class TournamentTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+    def setUp(self):
+        self.runners = {
+            'Usain': run_2.Runner('Usain', speed=10),
+            'Andrey': run_2.Runner('Andrey', speed=9),
+            'Nick': run_2.Runner('Nick', speed=3)
+        }
+
+    @classmethod
+    def tearDownClass(cls):
+        for place, runner in sorted(cls.all_results.items()):
+            print(f'Место {place}: {runner.name}')
+
+    def test_race_usain_andrey_nick(self):
+        tournament = run_2.Tournament(1540, self.runners['Usain'], self.runners['Andrey'], self.runners['Nick'])
+        results = tournament.start()
+        self.all_results.update(results)
+        last_runner = results[max(results.keys())]  # Самый последний бегун
+        self.assertTrue(last_runner.name == 'Nick')
+
+
+
+if __name__ == '__main__':
+    unittest.main()
